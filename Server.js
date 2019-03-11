c6fea87bacf32cb1bcff0aae1d863d8c0dd01ff3
Jeff Kwok <jkwok97@gmail.com>
2019-03-10 18:34:19 -0600

    fixing server

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/skymatics-test/index.html'));
 })
 
 app.listen(process.env.PORT || 8080);
