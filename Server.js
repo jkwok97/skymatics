7920bc0b64fdd37ba3a68ba9079288cc2bcc1a89
Jeff Kwok <jkwok97@gmail.com>
2019-03-10 20:14:57 -0600

    fixed server

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/skymatics-test'));
 
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/skymatics-test/index.html'));
